c9f5ed18f99db6d43c4802a351ac68b4124ee77e
Bernd Porr <mail@berndporr.me.uk>
2019-03-04 12:33:27 +0000

    Now also the reward_seen is calculated and used in the sim.

--- prism_model/run_models.py
+++ prism_model/run_models.py
@@ -19,6 +19,14 @@ shorter_food_delay = 10
 
 uncertainty = 10
 
+# activtiy if active
+
+# visual input if landmark can be seen
+vis1 = 1
+
+# visual input if reward shows up
+vis2 = 2
+
 def ofc5HTreceptors(_5ht,x,t):
     if t == 1:
         return (1-math.exp(-pow(x/(_5ht+1),(_5ht+1))))*(_5ht+2)
@@ -28,12 +36,16 @@ def ofc5HTreceptors(_5ht,x,t):
         return (1-math.exp(-pow(x/(_5ht+2+DRN_OFFSET),(_5ht+1+DRN_OFFSET))))*(_5ht+2+DRN_OFFSET)
 
 
-def doExperiment(delay,reward_unseen_speed,speed_uncertainty,reportfile,resultsfile):
+def doExperiment(delay,reward_unseen_speed,reward_seen_speed,speed_uncertainty,reportfile,resultsfile):
     # running the script
     delay = int(delay)
     reward_unseen_speed = int(reward_unseen_speed)
     speed_uncertainty = int(speed_uncertainty)
-    parameters = "delay={},reward_unseen_speed={},speed_uncertainty={}".format(delay,reward_unseen_speed,speed_uncertainty)
+    reward_seen_speed = int(reward_seen_speed)
+    parameters = "delay={},reward_unseen_speed={},reward_seen_speed={},speed_uncertainty={}".format(delay,
+                                                                                                    reward_unseen_speed,
+                                                                                                    reward_seen_speed,
+                                                                                                    speed_uncertainty)
     print("Running experiment with: "+parameters)
     reportfile.write("SCRIPT RUN:\n")
     try:
@@ -64,53 +76,66 @@ reportfile = open(logfilename, 'wt')
 resultsfile = open(pval_filename, 'wt')
 
 print("1 - Normal")
-reward_unseen_speed = 100 * ofc5HTreceptors(3, 1.0, 1)
+reward_unseen_speed = 100 * ofc5HTreceptors(3, vis1, 1)
+reward_seen_speed = 100 * ofc5HTreceptors(3, vis1+vis2, 1)
 doExperiment(standard_food_delay,
              reward_unseen_speed,
+             reward_seen_speed,
              reward_unseen_speed/uncertainty,
              reportfile,resultsfile)
 
 print("2 - Less delay")
 doExperiment(shorter_food_delay,
              reward_unseen_speed,
+             reward_seen_speed,
              reward_unseen_speed/uncertainty,
              reportfile,resultsfile)
 
 print("3 - DRN is suppressed")
-reward_unseen_speed = 100 * ofc5HTreceptors(0.3, 1.0, 1)
+reward_unseen_speed = 100 * ofc5HTreceptors(0.3, vis1, 1)
+reward_seen_speed = 100 * ofc5HTreceptors(0.3, vis1+vis2, 1)
 doExperiment(standard_food_delay,
              reward_unseen_speed,
+             reward_seen_speed,
              reward_unseen_speed/uncertainty,
              reportfile,resultsfile)
 
 print("4 - DRN is suppressed, less delay")
 doExperiment(shorter_food_delay,
              reward_unseen_speed,
+             reward_seen_speed,
              reward_unseen_speed/uncertainty,
              reportfile,resultsfile)
 
 print("5 - DRN is suppressed and SSRI")
-reward_unseen_speed = 100 * ofc5HTreceptors(0.3, 1.0, 3)
+reward_unseen_speed = 100 * ofc5HTreceptors(0.3, vis1, 3)
+reward_seen_speed = 100 * ofc5HTreceptors(0.3, vis1+vis2, 3)
 doExperiment(standard_food_delay,
              reward_unseen_speed,
+             reward_seen_speed,
              reward_unseen_speed/uncertainty,
              reportfile,resultsfile)
 
 print("6 - DRN is suppressed and SSRI, less delay")
 doExperiment(shorter_food_delay,
              reward_unseen_speed,
-             reward_unseen_speed/uncertainty,reportfile,resultsfile)
+             reward_seen_speed,
+             reward_unseen_speed/uncertainty,
+             reportfile,resultsfile)
 
 print("7 - DRN is suppressed and 5HTR2 up")
-reward_unseen_speed = 100 * ofc5HTreceptors(0.3, 1.0, 2)
+reward_unseen_speed = 100 * ofc5HTreceptors(0.3, vis1, 2)
+reward_seen_speed = 100 * ofc5HTreceptors(0.3, vis1+vis2, 2)
 doExperiment(standard_food_delay,
              reward_unseen_speed,
+             reward_seen_speed,
              reward_unseen_speed/uncertainty,
              reportfile,resultsfile)
 
 print("8 - DRN is suppressed and 5HTR2 up, less delay")
 doExperiment(shorter_food_delay,
              reward_unseen_speed,
+             reward_seen_speed,
              reward_unseen_speed/uncertainty,
              reportfile,resultsfile)
 
